c2b9b6f4b2df3870a4056109bcca293a89a768e3
okysetiawan <okyng08@gmail.com>
2023-04-15 23:38:02 +0700

    add comment on abstraction

--- writer.go
+++ writer.go
@@ -3,12 +3,14 @@ package document
 import "bytes"
 
 type (
+	// Builder is interface implemented by an object that can handle build Document before process write file
 	Builder interface {
 		CreateSheet(sheetName string, headers ...string) Builder
 		AddRow(cells ...any) Builder
 		Writer
 	}
 
+	// Writer is interface implemented by an object that can handle Write Document.
 	Writer interface {
 		Buffer() (*bytes.Buffer, error)
 		Save(path string) (err error)
